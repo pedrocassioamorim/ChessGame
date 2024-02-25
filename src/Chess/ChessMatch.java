521ffe295db2d060a68f25ecb0fe9f9270a710a1
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-02-25 11:46:29 -0300

    Ajustes UI, parte 1

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,12 +1,17 @@
 package Chess;
 
 import Boardgame.Board;
+import Boardgame.Piece;
+import Boardgame.Position;
+import Chess.pieces.King;
+import Chess.pieces.Rook;
 
 public class ChessMatch {
     private Board board;
 
     public ChessMatch(){
         board = new Board(8,8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces() {
@@ -17,5 +22,49 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+        Position source = sourcePosition.toPosition();
+        Position target = targetPosition.toPosition();
+        validateSourcePosition(source);
+        Piece capturedPiece = makeMove(source, target);
+        return (ChessPiece) capturedPiece;
+    }
+
+    private void validateSourcePosition(Position position){
+        if(!board.thereIsAPiece(position)){
+            throw new ChessException("There is no Piece on source position");
+        }
+    }
+
+    private Piece makeMove(Position source, Position target){
+        Piece p = board.removePiece(source);
+        Piece capturedPiece = board.removePiece(target);
+        board.placePiece(p, target);
+        return capturedPiece;
+    }
+
+
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+    private void initialSetup(){
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+//        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+//        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+//        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+//        placeNewPiece('d', 1, new King(board, Color.WHITE));
+//
+//        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+//        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+//        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+//        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+//        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+//        placeNewPiece('d', 8, new King(board, Color.BLACK));
     }
 }
--- src/application/Program.java
+++ src/application/Program.java
@@ -2,15 +2,47 @@ package application;
 
 import Boardgame.Board;
 import Boardgame.Position;
+import Chess.ChessException;
 import Chess.ChessMatch;
+import Chess.ChessPiece;
+import Chess.ChessPosition;
+
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
 
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+
+        while (true) {
+            try {
+                UI.clearScrean();
+                UI.cleanScrean();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+                System.out.println();
+                System.out.print("Target:");
+                System.out.println();
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+            }
+            catch (ChessException | InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            } catch (IOException | InterruptedException e) {
+                System.out.println(e.getMessage());
+                throw new RuntimeException(e);
+            }
+        }
 
 
 
